e262c457e28ad6c3d43aa05c0e9955e0a640f960
Matthew Walker <mwalker.za@gmail.com>
2021-05-03 19:19:11 +0200

    COS2614 q1

--- COS2614/assignment1/question1/main.cpp
+++ COS2614/assignment1/question1/main.cpp
@@ -9,15 +9,19 @@ int main (int argc, char* argv[])
     do {
         double celciusInput, fahrenheit;
 
-        celciusInput = QInputDialog::getDouble(0, "Temperature Converter",
+        //Request temp from user through QInputDialog.
+        celciusInput = QInputDialog::getDouble(0, "Temp Converter",
             "Enter temperature in celsius:", 0);
 
+        //Convert from °C to °F using the given formula.
         fahrenheit = ((9 * celciusInput) / 5) + 32;
 
+        //Concatenate the °C and °F arguments into a string.
         QString response = QString("The converted temperature of %1°C is %2°F.\n%3")
             .arg(celciusInput).arg(fahrenheit).arg("Convert another temperature?");
 
-        output = QMessageBox::question(0, "Temperature Converter", response,
+        //Output converted temp through QMessageBox.
+        output = QMessageBox::question(0, "Temp Converter", response,
             QMessageBox::Yes | QMessageBox::No);
     } while (output == QMessageBox::Yes);
     return EXIT_SUCCESS;
