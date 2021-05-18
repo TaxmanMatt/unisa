2e6ba2f755460514715fac1378db25eb27e98129
Matthew Walker <mwalker.za@gmail.com>
2021-05-18 19:15:48 +0200

    COS1512 - ass2q3

--- COS1512/Assignment 2/question3/question3.cpp
+++ COS1512/Assignment 2/question3/question3.cpp
@@ -4,15 +4,31 @@
 using namespace std;
 
 double calcAverage(double weight1, double mark1, double weight2, double mark2);
+void writeFile(string subjectCode, double average);
 
 int main()
 {
-    cout << fixed;
-    cout.precision(2);
-    cout << calcAverage(30, 66, 70, 49) << "%";
+    ifstream inputFile("assignments.dat");
+    string subjectCode;
+    double weight1, mark1, weight2, mark2, average;
+
+    while (inputFile >> subjectCode >> weight1 >> mark1 >> weight2 >> mark2) {
+        average = calcAverage(weight1, mark1, weight2, mark2);
+        writeFile(subjectCode, average);
+    }
+
+    cout << "Average has been written to yearmark.dat" << endl;
+
+    return 0;
 }
 
 double calcAverage(double weight1, double mark1, double weight2, double mark2) {
-    ifstream inputFile("assignments.dat");
     return ((mark1 / 100) * weight1) + ((mark2 / 100) * weight2);
+}
+
+void writeFile(string subjectCode, double average) {
+    ofstream outputFile("yearmark.dat", std::ios_base::app);
+    outputFile << fixed;
+    outputFile.precision(2);
+    outputFile << subjectCode << " " << average << "%" << endl;
 }
