e72a02bb8b1eead8b3186a3ab25a55c223d0ebde
Matthew Walker <mwalker.za@gmail.com>
2021-05-20 08:50:58 +0200

    practice

--- COS1512/Assignment 2/question4/question4.cpp
+++ COS1512/Assignment 2/question4/question4.cpp
@@ -18,9 +18,67 @@ string outputFunc() {
 
     //while (getline(inputFile, checkEachChar)) {
     while (inputFile >> checkEachChar) {
+        if (checkEachChar == '0') {
+            checkEachChar = 's';
+        } else
+        if (checkEachChar == '1') {
+            checkEachChar = 'g';
+        } else
         if (checkEachChar == '2') {
             checkEachChar = 'o';
+        } else
+        if (checkEachChar == '3') {
+            checkEachChar = 'y';
+        } else
+        if (checkEachChar == '4') {
+            checkEachChar = 'v';
+        } else
+        if (checkEachChar == '5') {
+            checkEachChar = 'n';
+        } else
+        if (checkEachChar == '6') {
+            checkEachChar = 'f';
+        } else
+        if (checkEachChar == '7') {
+            checkEachChar = 'j';
         }
+/*        switch (checkEachChar) {
+            case 0:
+                checkEachChar = 's';
+                outputFile << checkEachChar;
+                break;
+            case 1:
+                checkEachChar = 'g';
+                outputFile << checkEachChar;
+                break;
+            case 2:
+                checkEachChar = 'o';
+                outputFile << checkEachChar;
+                break;
+            case 3:
+                checkEachChar = 'y';
+                outputFile << checkEachChar;
+                break;
+            case 4:
+                checkEachChar = 'v';
+                outputFile << checkEachChar;
+                break;
+            case 5:
+                checkEachChar = 'n';
+                outputFile << checkEachChar;
+                break;
+            case 6:
+                checkEachChar = 'f';
+                outputFile << checkEachChar;
+                break;
+            case 7:
+                checkEachChar = 'j';
+                outputFile << checkEachChar;
+                break;
+            default:
+                outputFile << checkEachChar;
+                break;
+        }*/
         outputFile << checkEachChar;
     }
 }
@@ -44,13 +102,3 @@ int main()
 
     return 0;
 }
-
-/*
-//while (getline(inputFile, checkEachChar)) {
-char ReplaceCharacter() {
-    char in, replace;
-
-    while (inputFile >> checkEachChar) {
-        if (checkEachChar == '2') {
-            checkEachChar = 'o';
-*/
