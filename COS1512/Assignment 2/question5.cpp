cc7885789fccd5a3878a724eaf8be9ab110fdc1d
Matthew Walker <mwalker.za@gmail.com>
2021-05-21 10:39:41 +0200

    COS1512 - ass3q5

--- COS1512/Assignment 2/question5.cpp
+++ COS1512/Assignment 2/question5.cpp
@@ -3,15 +3,58 @@
 
 using namespace std;
 
+void fullName();
+void noMiddleName();
+
+
 int main()
 {
+    while(true) {
+        char choice;
+        cout << "Do you have a middle name? Enter y or n: ";
+        cin >> choice;
 
+        switch(choice) {
+            case 'Y':
+            case 'y':
+                fullName();
+                break;
+            case 'N':
+            case 'n':
+                noMiddleName();
+                break;
+            default:
+                cout << "Error: Please choose y or n." << endl << endl;
+                continue;
+        } break;
+    }
+
+    return 0;
+}
+
+void fullName() {
     char firstName[20], middleName[20], surname[20];
 
     cout << "Enter your name, middle name and surname: ";
     cin >> firstName >> middleName >> surname;
-
     cout << surname << ", " << firstName << " " << middleName[0] << ". ";
+}
 
-    return 0;
+void noMiddleName() {
+    char firstName[20], surname[20];
+
+    cout << "Enter your name and surname: ";
+    cin >> firstName >> surname;
+
+
+    if (islower(firstName[0])) {
+        putchar(toupper(firstName[0]));
+        for (int i = 1; i < strlen(firstName); i++) {
+                putchar(tolower(firstName[i]));
+        }
+    } else
+        putchar(firstName[0]);
+        for (int i = 1; i < strlen(firstName); i++) {
+            putchar(tolower(firstName[i]));
+        }
 }
